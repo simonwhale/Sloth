3818a0b2b8c96a5e56b6de5780b53160aab926c9
simonwhale <simonwhale@live.com>
2020-11-22 22:05:08 +0000

    Add files via upload

---
+++ SlothEnterprise.ProductApplication.Tests/Models/ApplicationResult.cs
@@ -0,0 +1,14 @@
+﻿using SlothEnterprise.External;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlothEnterprise.ProductApplication.Tests.Models
+{
+    public class ApplicationResult : IApplicationResult
+    {
+        public int? ApplicationId { get; set; }
+        public bool Success { get; set; }
+        public IList<string> Errors { get; set; }
+    }
+}
